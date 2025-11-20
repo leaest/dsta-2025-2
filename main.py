ed2021c5a4b60a6af6ef80fdd7728b9fbbcd803d
leaest <lea.estermann@stud.unilu.ch>
2025-11-20 21:00:59 +0100

    Creation & testing of module 2 (build neural network)
    - Created  inside  so the folder is recognized as a module
    - Added  module to create the NN
    - Updated  to import and call the function from  and print model summary for testing (model is built and  runs without errors)

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from data_prep import load_data
+from scr.data_prep import load_data
 
 x_train, y_train, x_test, y_test = load_data()
 print("x_train shape:", x_train.shape)
 print("y_train shape:", y_train.shape)
 print("x_test shape:", x_test.shape)
 print("y_test shape:", y_test.shape)
+
+
+from scr.neuralnet_architecture import build_neuralnet
+
+model = build_neuralnet()
+model.summary()
 
---
+++ scr/neuralnet_architecture.py
@@ -0,0 +1,19 @@
+import keras
+from keras import layers
+
+def build_neuralnet(input_shape=(28, 28, 1), num_classes=10):
+    
+    model = keras.Sequential(
+        [
+            keras.Input(shape=input_shape),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dropout(0.5),
+            layers.Dense(num_classes, activation="softmax")
+        ]
+    )
+
+    return model
