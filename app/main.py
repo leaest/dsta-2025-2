8e3c07ca49286bb7675796c92592d15d8c8723c4
leaest <lea.estermann@stud.unilu.ch>
2026-01-12 18:17:39 +0100

    Basic stucture for app/main.py
    Tried to setup a basic structure for the main.py inside the app folder to expose a REST endpoint and accept POST request. Not working at this point

--- app/main.py
+++ app/main.py
@@ -1,54 +1,23 @@
-import numpy as np
-from tensorflow.keras.datasets import mnist
+from flask import Flask, request, jsonify
+from .src.image_utils import deserialize_image
+from .src.model_utils import load_model
+from .src.db_utils import init_tables
 
-from src.db_utils import create_database_if_not_exists, init_tables, connect_milestone_db
-from src.model_utils import load_model
-from src.image_utils import serialize_image, deserialize_image
+app = Flask(__name__)
 
-# 1. Ensure database exists
-create_database_if_not_exists()
+# Load model once
+model = load_model("models/CNN_model.keras")
 
-# 2. Ensure tables exist
+# Initialize DB tables
 init_tables()
 
-# 3. Load model
-model = load_model()
+@app.route("/")
+def home():
+    return "Prediction service is running"
 
-# 4. Load one sample image
-(_, _), (x_test, y_test) = mnist.load_data()
-sample_img = x_test[0]
-true_label = y_test[0]
+@app.route("/predict", methods=["POST"])
+def predict():
 
-# Preprocess for Neural Net
-sample_img_processed = sample_img.reshape(1, 28, 28, 1).astype("float32") / 255
 
-# Serialize image
-img_serialized = serialize_image(sample_img)
-
-# Insert into DB
-conn = connect_milestone_db()
-cur = conn.cursor()
-
-cur.execute(
-    "INSERT INTO input_data (image) VALUES (%s) RETURNING id;",
-    (img_serialized,)
-)
-input_id = cur.fetchone()[0]
-conn.commit()
-
-# Predict
-pred = model.predict(sample_img_processed)
-pred_label = int(np.argmax(pred))
-
-# Store prediction with foreign key
-cur.execute(
-    "INSERT INTO predictions (input_id, predicted_label) VALUES (%s, %s);",
-    (input_id, pred_label)
-)
-conn.commit()
-
-print("Saved input id:", input_id)
-print("Predicted:", pred_label, "| True:", true_label)
-
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
